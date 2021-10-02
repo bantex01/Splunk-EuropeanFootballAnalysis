d06797a77548f4f9a1b66d54635cd9df9e8db7c1
Alex Dalton <alexfdalton@hotmail.com>
2021-10-02 09:02:18 +0100

    latest version

--- splunk_btb/bin/gather_data.py
+++ splunk_btb/bin/gather_data.py
@@ -9,6 +9,7 @@ import sys
 import json
 import time
 import ntpath
+from io import open
 
 from collections import defaultdict
 
@@ -34,14 +35,14 @@ def parse_data_source():
 				sources.append(conf_line.rstrip())
 
 	else:
-		print "File does not exist"
+		print("File does not exist")
 		exit(2)
 
 
 def clear_splunk_index():
 
 	try:
-		curl_cmd = "/usr/bin/curl -m 10 -k -s -u admin:changeme -d \"search=search index=football_19_20 earliest_time=1 latest_time=\"08/31/2030:20:00:00\" | delete\" -d output_mode=json https://localhost:8089/services/search/jobs"
+		curl_cmd = "/usr/bin/curl -m 10 -k -s -u admin:SarahLee01 -d \"search=search index=football_21_22 earliest_time=1 latest_time=\"08/31/2030:20:00:00\" | delete\" -d output_mode=json https://localhost:8089/services/search/jobs"
 
 		out = subprocess.check_output([curl_cmd], shell=True, stderr=subprocess.STDOUT)
 		sid_dict = json.loads(out)
@@ -50,7 +51,7 @@ def clear_splunk_index():
 		time.sleep(5)
 
 		try:
-			curl_cmd = "/usr/bin/curl -m 10 -k -s -u admin:changeme https://localhost:8089/services/search/jobs/" + sid
+			curl_cmd = "/usr/bin/curl -m 10 -k -s -u admin:SarahLee01 https://localhost:8089/services/search/jobs/" + sid
 			out = subprocess.check_output([curl_cmd], shell=True, stderr=subprocess.STDOUT)
 			for line in out.splitlines():
 				if (re.match(r'.*dispatchState\">\w+<.*', line)):
@@ -58,72 +59,83 @@ def clear_splunk_index():
 					break
 
 			if (dispatchState == "DONE"):
-				print "Search seems to have gone through"
+				print("Search seems to have gone through")
 			else:
-				print "Check index, something went wrong"
+				print("Check index, something went wrong")
 				exit(2)
 
-		except Exception, e:
-			print "exception caught checking status of search job "+str(e)
+		except Exception as e:
+			print("exception caught checking status of search job "+str(e))
 
-	except Exception, e:
-		print "exception caught submiting deletion search "+str(e)
+	except Exception as e:
+		print("exception caught submiting deletion search "+str(e))
 
 
 def grab_and_ingest_csv_files():
 
 	for csv in sources:
-		print "gathering "+csv
+		print( "gathering "+csv)
 		try:
 			wget_cmd = "wget "+csv
 			out = subprocess.check_output([wget_cmd], shell=True, stderr=subprocess.STDOUT)
 			#print out
 			csv_file = os.path.basename(csv)
-			print csv_file
+			print(csv_file)
 
 			if ("200 OK") not in out:
-				print csv + " not found, removing dummy file"
+				print(csv + " not found, removing dummy file")
 				try:
 					rm_cmd = "rm -fr "+str(csv_file)
 					out = subprocess.check_output([rm_cmd], shell=True, stderr=subprocess.STDOUT)
-				except Exception, e:
-					print "problem removing "+str(csv)
+				except Exception as e:
+					print("problem removing "+str(csv))
 
 			else:
-				sed_cmd = "cat " + csv_file + " | sed 's/>/gt/g' | sed 's/</lt/g' > " + csv_file + ".1" 
-				print sed_cmd
-				print "Successful gather of "+str(csv_file)
+				if (re.match("EC.csv", csv_file)):
+					print("found conference file, converting")
+					with open("EC.csv", 'r', encoding='cp1252') as inp, open("EC.csv.temp", 'w', encoding='utf-8') as outp:
+						for line in inp:
+							outp.write(line)
+
+					mv_cmd = "mv EC.csv.temp EC.csv"
+					print("moving EC temp file to EC.csv")
+					mv_run = subprocess.check_output([mv_cmd], shell=True, stderr=subprocess.STDOUT)
+
+
+				sed_cmd = "cat " + csv_file + " | sed 's/>/gt/g' | sed 's/</lt/g' > " + csv_file + ".1"
+				print(sed_cmd)
+				print("Successful gather of "+str(csv_file))
 
 				sed_run = subprocess.check_output([sed_cmd], shell=True, stderr=subprocess.STDOUT)
 				if (os.path.exists(csv_file + ".1")):
-					print "amended file exists"
+					print("amended file exists")
 					os.remove(csv_file)
-					os.rename(csv_file +".1",csv_file)	
+					os.rename(csv_file +".1",csv_file)
 				else:
-					print "something went wrong with the sed efforts"
+					print("something went wrong with the sed efforts")
 					exit(2)
 
-				oneshot_cmd = "/opt/splunk/bin/splunk add oneshot " +str(csv_file) + " -sourcetype footie_csv -index football_19_20 -auth admin:changeme"
+				oneshot_cmd = "/Applications/Splunk/bin/splunk add oneshot " +str(csv_file) + " -sourcetype footie_csv -index football_21_22 -auth admin:SarahLee01"
 				time.sleep(5)
 				try:
 					out = subprocess.check_output([oneshot_cmd], shell=True, stderr=subprocess.STDOUT)
 					if "added" in out:
-						print str(csv_file) + " added successfully, removing file"
+						print(str(csv_file) + " added successfully, removing file")
 						rm_cmd = "rm -fr "+str(csv_file)
 						out = subprocess.check_output([rm_cmd], shell=True, stderr=subprocess.STDOUT)
-				except Exception, e:
-					print "problem ingesting " +str(csv) + " review output, aborting"
+				except Exception as e:
+					print("problem ingesting " +str(csv) + " review output, aborting")
 					exit(2)
 
-		except Exception, e:
-			print "problem gathering "+csv + " exception is "+str(e)
+		except Exception as e:
+			print("problem gathering "+csv + " exception is "+str(e))
 
 
 
 # Main
-print "Parsing sources file to ascertain csv list..."
+print("Parsing sources file to ascertain csv list...")
 parse_data_source()
-print "clearing splunk index, ready for new ingest..."
+print("clearing splunk index, ready for new ingest...")
 clear_splunk_index()
-print "Grabbing new CSV files..."
+print("Grabbing new CSV files...")
 grab_and_ingest_csv_files()
